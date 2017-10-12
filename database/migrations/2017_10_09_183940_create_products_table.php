c6ca69b4bb1d821e20ef15eb51c90ea43b99fee6
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-10-12 00:40:24 -0500

    Nulllable para position de productos

--- database/migrations/2017_10_09_183940_create_products_table.php
+++ database/migrations/2017_10_09_183940_create_products_table.php
@@ -19,7 +19,7 @@ class CreateProductsTable extends Migration
             $table->string('name');
             $table->text('description');
             $table->string('image')->nullable();
-            $table->integer('position');
+            $table->integer('position')->nullable();
             $table->timestamps();
         });
     }
