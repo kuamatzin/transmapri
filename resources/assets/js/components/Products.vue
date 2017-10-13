fcd3fcf57dda1f58ad30108dcedb90428adbdda2
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-10-13 13:49:13 -0500

    Imagen en modal

--- resources/assets/js/components/Products.vue
+++ resources/assets/js/components/Products.vue
@@ -39,7 +39,7 @@
                 <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                   <div class="modal-dialog modal-lg">
                     <div class="modal-content">
-                      <img class="card-img-top" src="/images/equipo/carros/ambulancia.jpg" alt="Card image cap">
+                      <img class="card-img-top" :src="'/storage/' + product.image" alt="Card image cap">
                       <br>
                       <p class="text-center">Carro</p>
                     </div>
