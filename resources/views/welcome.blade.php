2829c60ae745100a3bf71ab9a7c4324cbb0ec15c
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-10-14 14:47:54 -0500

    Imagen backgrond fondo blanco

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,7 +57,7 @@
                 </ol>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img class="d-block w-100" src="/images/backg1.png" alt="First slide">
+                        <img class="d-block w-100" src="/images/backg11.png" alt="First slide">
                     </div>
                     <div class="carousel-item">
                         <img class="d-block w-100" src="/images/backg2.png" alt="Second slide">
